4c6deef182f667d0e7e25e8f18aa95f39f103946
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-07-25 10:26:29 -0400

    Time: 23 ms (51.70%), Space: 13.4 MB (29.38%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        visited = set()
+        
+        while n != 1:
+            sum_of_squares = 0
+            
+            while n > 0:
+                digit = n % 10
+                sum_of_squares += digit ** 2
+                n //= 10
+                
+            if sum_of_squares in visited:
+                return False
+            
+            visited.add(sum_of_squares)
+            
+            n = sum_of_squares
+            
+        return True
