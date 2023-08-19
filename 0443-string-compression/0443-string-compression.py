c827404877d8bdeefefb789c69785f7e281d5ac5
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-08-19 10:32:58 -0400

    Time: 34 ms (85.04%), Space: 13.8 MB (10.22%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+
+        if len(chars) <= 1:
+            return len(chars)
+
+        write = 0
+        read = 0
+
+        while read < len(chars):
+            current_char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == current_char:
+                read += 1
+                count += 1
+
+            chars[write] = current_char
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+
+        return write
