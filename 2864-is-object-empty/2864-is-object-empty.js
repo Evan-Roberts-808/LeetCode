e8ed660780d8365510c865e3e47334c1eed6414f
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-10-15 10:56:11 -0400

    Time: 48 ms (76.47%), Space: 42.8 MB (33.78%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if (Array.isArray(obj)) {
-        return obj.length === 0;
-    } else if (typeof obj === 'object' && obj !== null) {
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    return false;
+    for (const _ in obj) return false;
+    return true;
 };
