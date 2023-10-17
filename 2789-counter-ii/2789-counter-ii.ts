61404e4673ac22d8128dba58b0e2f766a4372e0b
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-10-17 16:51:24 -0400

    Time: 67 ms (33.72%), Space: 44.6 MB (94.39%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.ts
+++ 2789-counter-ii/2789-counter-ii.ts
@@ -25,19 +25,3 @@ function createCounter(init: number): ReturnObj {
  * counter.reset(); // 5
  * counter.decrement(); // 4
  */
-
-//  let currentValue = init
-
-//     return {
-//         increment:function(){
-//             return currentValue += 1
-//         },
-
-//         decrement:function(){
-//             return currentValue -= 1
-//         },
-
-//         reset:function(){
-//             return currentValue = init
-//         }
-//     }
