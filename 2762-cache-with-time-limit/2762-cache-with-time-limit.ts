b3bb4c2cd4343a8de096a57ce6fcb7b861ed2dc2
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-10-27 16:12:23 -0400

    Time: 68 ms (14.8%), Space: 42.7 MB (66.21%) - LeetHub

---
+++ 2762-cache-with-time-limit/2762-cache-with-time-limit.ts
@@ -0,0 +1,50 @@
+class TimeLimitedCache {
+    cache: Map<number, [number, number]>
+    
+    constructor() {
+        this.cache = new Map();
+    }
+    
+    set(key: number, value: number, duration: number): boolean {
+        const now = Date.now();
+        if (this.cache.has(key)) {
+            this.cache.set(key, [value, now + duration])
+            return true;
+        }
+        this.cache.set(key, [value, now + duration])
+        return false;
+    }
+
+    get(key: number): number {
+        const now = Date.now();
+        if (this.cache.has(key)) {
+            const [value, expiry] = this.cache.get(key)!;
+            if (now < expiry) {
+                return value;
+            } else {
+                this.cache.delete(key);
+            }
+        }
+        return -1;
+    }
+
+    count(): number {
+        const now = Date.now();
+        let count = 0;
+        for (const [key, [_, expiry]] of this.cache) {
+            if (now < expiry) {
+                count++;
+            } else {
+                this.cache.delete(key);
+            }
+        }
+        return count;
+    }
+}
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
