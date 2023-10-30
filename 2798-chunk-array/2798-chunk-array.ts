26a01511094b3b02f0caaa9d9b96b924ad964940
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-10-30 11:47:34 -0400

    Create 2798-chunk-array.ts

---
+++ 2798-chunk-array/2798-chunk-array.ts
@@ -0,0 +1,27 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+	const chunked : Obj[][] = []
+    let index : number = 0
+
+    while (index < arr.length) {
+        chunked.push(arr.slice(index, size + index))
+        index += size
+    }
+    
+    return chunked
+
+};
+
+// var chunk = function(arr, size) {
+//     const chunked = []
+//     let index = 0
+
+//     while (index < arr.length) {
+//         chunked.push(arr.slice(index, size + index))
+//         index += size
+//     }
+    
+//     return chunked
+// };
