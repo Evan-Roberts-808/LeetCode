55432f457ab3a39ec6fd0161fa7be6f39661d3b2
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2023-11-02 13:23:04 -0400

    Update 0389-find-the-difference.java

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,22 +1,10 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> counter = new HashMap<>();
-
+        int charCode = (int) t.charAt(s.length());
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+            charCode -= (int) s.charAt(i);
+            charCode += (int) t.charAt(i);
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
-                return ch;
-            }
-            counter.put(ch, counter.get(ch) - 1);
-        }
-        return ' ';
+        return (char) charCode;
     }
 }
