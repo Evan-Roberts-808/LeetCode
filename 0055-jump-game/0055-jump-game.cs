0a6cccdd88fbeb017a66f1d8d79dae9b9459b582
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2024-02-19 11:13:44 -0500

    Time: 194 ms (93.34%), Space: 62.2 MB (24.35%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int n = nums.Length;
+        int maxReach = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > maxReach)
+            {
+                return false;
+            }
+            maxReach = Math.Max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1)
+            {
+                return true;
+            }
+        }
+        return true;
+    }
+}
