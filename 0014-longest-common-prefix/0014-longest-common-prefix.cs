821395c9bdad27910626b3b75dcd40367318f73b
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2024-02-29 11:55:59 -0500

    Time: 64 ms (80.72%), Space: 42 MB (84.20%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -0,0 +1,40 @@
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0)
+        {
+            return "";
+        }
+
+        // Check for empty strings
+        if (strs.Any(str => str.Length == 0))
+        {
+            return "";
+        }
+
+        string reference = strs[0];
+
+        if (!strs.All(str => str[0] == reference[0]))
+        {
+            return "";
+        }
+
+        int shortest = strs.Min(s => s.Length);
+        int i = 1;
+
+        while (i < shortest && i < reference.Length)
+        {
+            foreach (string str in strs)
+            {
+                if (i >= str.Length || str[i] != reference[i])
+                {
+                    return reference.Substring(0, i);
+                }
+            }
+            i++;
+        }
+
+        return reference.Substring(0, shortest);
+    }
+}
