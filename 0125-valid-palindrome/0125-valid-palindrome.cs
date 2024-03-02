c9f60552478539606a2da6f88c71adb08345b5d3
Evan Roberts <105817690+Evan-Roberts-808@users.noreply.github.com>
2024-03-02 17:29:29 -0500

    Time: 68 ms (37.57%), Space: 46.5 MB (22.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -0,0 +1,21 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string polishedString = Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
+        
+        if(polishedString == "")
+        {
+            return true;
+        }
+        
+        char[] charArray = polishedString.ToCharArray();
+        Array.Reverse(charArray);
+        string reverseString = new string(charArray);
+        
+        Console.WriteLine(polishedString);
+        Console.WriteLine(reverseString);
+        
+        return polishedString == reverseString ? true : false;
+    }
+}
